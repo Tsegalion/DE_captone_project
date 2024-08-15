77c5ab68e4071e43aee4c911c8700943d430b8ca
tsegalion <tyebunoluwa@gmail.com>
2024-08-15 23:32:25 +0100

    update final model

--- dbt_bigquery/models/final/fct_avg_delivery_time.sql
+++ dbt_bigquery/models/final/fct_avg_delivery_time.sql
@@ -2,7 +2,6 @@
 {{ config(materialized='table') }}
 
 SELECT
-    order_id,
-    delivery_time_days
+    ROUND(AVG(delivery_time_days)) AS avg_delivery_time_days
 FROM
     {{ ref('int_avg_delivery_time') }}
